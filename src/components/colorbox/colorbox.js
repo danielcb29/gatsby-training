4869f76d3d5d24af69050585c4c23b64f7ce718d
danielcb29 <danielc.web@gmail.com>
2019-08-28 14:57:10 -0500

    Scroll animation test

--- src/components/colorbox/colorbox.js
+++ src/components/colorbox/colorbox.js
@@ -2,6 +2,7 @@ import { ScrollAnimation } from '../../utils/scrollAnimation'
 
 class ColorBoxComponent {
     constructor() {
+        console.log("hello world!");
         new ScrollAnimation('box', 'component--fadeout', 'component--fadein');
     }
 }
